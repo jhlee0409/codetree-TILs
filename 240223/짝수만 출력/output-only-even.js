2af2a43ef2697b30127e9548c79cabb5c259fbfa
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-23 17:21:34 +0900

    Title: 짝수만 출력, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240223/짝수만 출력/output-only-even.js
@@ -0,0 +1,4 @@
+let fs = require('fs')
+let [s,e] = fs.readFileSync(0).toString().split(' ').map(v=> +v)
+const r = Array.from({length: Math.ceil((e-s+1)/2)}, (_,v)=> 2*v+s).join(' ')
+console.log(r)
