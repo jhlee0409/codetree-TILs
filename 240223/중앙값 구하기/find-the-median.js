4ddc588d84c75e97ff954fb2bcc62b99ee7c1965
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-23 15:40:18 +0900

    Title: 중앙값 구하기, Time: 229ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240223/중앙값 구하기/find-the-median.js
+++ 240223/중앙값 구하기/find-the-median.js
@@ -1,26 +1,29 @@
 let fs = require('fs')
 let [a, b, c] = fs.readFileSync(0).toString().split(' ').map(v=> +v)
 
-const r = () => {
-let temp  = a
+let temp = a
 
-    if(b > temp) {
-        temp = temp 
-        if(temp > c) {
-            temp = temp
+if(temp > b) {
+    if(temp < c) {
+    }  else {
+        if(c < a) {
+            temp = c
         } else {
             temp = a
         }
+    }
+} else {
+    temp = b
+    if(temp < c) {
+
     } else {
-        temp = b
-        if(temp > c) {
-            temp = c
-        }  else {
+        temp = c
+        if(temp > a) {
+
+        } else {
             temp = a
         }
     }
-    return temp
 }
 
-
-console.log(r())
+console.log(temp)
