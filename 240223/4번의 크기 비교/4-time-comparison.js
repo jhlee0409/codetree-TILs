db25c71f43dd2f5291ede0f1d912d26126eb0e75
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-23 15:10:12 +0900

    Title: 4번의 크기 비교, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

--- 240223/4번의 크기 비교/4-time-comparison.js
+++ 240223/4번의 크기 비교/4-time-comparison.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
 let [a,b] = fs.readFileSync(0).toString().split('\n')
 
-const r = b.split(' ').reduce((acc,cur)=> a > cur ? acc += '1\n' : acc +=  '0\n'  ,'')
+const r = b.split(' ').reduce((acc,cur)=> +a > +cur ? acc += '1\n' : acc +=  '0\n'  ,'')
 console.log(r)
