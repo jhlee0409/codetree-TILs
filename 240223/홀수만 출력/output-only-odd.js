734f238ea8f4c946a4434932d07f05590f851fab
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-23 17:00:16 +0900

    Title: 홀수만 출력, Time: 197ms, Memory: 14MB, Status: Passed - CodeTree

--- 240223/홀수만 출력/output-only-odd.js
+++ 240223/홀수만 출력/output-only-odd.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let [s,e] = fs.readFileSync(0).toString().split(' ').map(v=> +v)
 
-const r = Array.from({length: Math.ceil(e/2)}, (_,v)=> 2*v+1).join(' ')
+const r = Array.from({length: Math.ceil((e-s+1)/2)}, (_,v)=> 2*v+s).join(' ')
 
 
 console.log(r)
