4315010bb2a644f6d1675cb92c86e6f26e582422
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-26 14:03:31 +0900

    Title: 등장횟수 확인, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240226/등장횟수 확인/check-number-of-appearances.js
+++ 240226/등장횟수 확인/check-number-of-appearances.js
@@ -1,6 +1,5 @@
 let fs = require('fs')
-let arr = fs.readFileSync(0).toString().split('\n').map(v=>+v)
-
-const r = arr.filter(i=> i % 2 === 0).length
+let arr = fs.readFileSync(0).toString().split('\n')
+const r = arr.filter(i=> +i % 2 === 0).length
 
 console.log(r)
