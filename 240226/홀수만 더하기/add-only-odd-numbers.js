ba9626ff402cba60b9c2d3e56e4db4021a0fb886
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-26 14:31:45 +0900

    Title: 홀수만 더하기, Time: 249ms, Memory: 14MB, Status: Passed - CodeTree

--- 240226/홀수만 더하기/add-only-odd-numbers.js
+++ 240226/홀수만 더하기/add-only-odd-numbers.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-let arr = fs.readFileSync(0).toString().split('\n').map(v=>+v)
+let [e,...arr] = fs.readFileSync(0).toString().split('\n').map(v=>+v)
 
-const r = arr.reduce((acc,cur)=> acc += cur % 2 === 1 && cur % 3 ===0 ? cur : 0 ,0)
+const r = arr.reduce((acc,cur) => acc += cur % 2 === 1 && cur % 3 === 0 ? cur : 0 ,0)
 console.log(r)
