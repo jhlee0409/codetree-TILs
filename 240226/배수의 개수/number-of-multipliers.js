7622c1df390756e5415246b4920a79cb3253a927
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-26 11:32:14 +0900

    Title: 배수의 개수, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

--- 240226/배수의 개수/number-of-multipliers.js
+++ 240226/배수의 개수/number-of-multipliers.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-let arr = fs.readFileSync(0).toString().split('\n').map(i=>+i)
+let arr = fs.readFileSync(0).toString().trim().split('\n').map(i=>+i)
 
 const t = arr.filter(i=> i % 3 === 0).length
 const f = arr.filter(i=> i % 5 === 0).length
