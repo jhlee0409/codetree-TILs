dd87d7a4413547ad5129def79b16721bc0c99aac
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-27 16:18:45 +0900

    Title: 특정 조건 배수의 합, Time: 186ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240227/특정 조건 배수의 합/sum-of-specific-condition-multiple.js
+++ 240227/특정 조건 배수의 합/sum-of-specific-condition-multiple.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let [a, b] = fs.readFileSync(0).toString().split(' ').map(v=>+v)
 
-const arr = Array.from({length: b - a + 1}, (_,v)=> v+a).filter(i => i % 5 === 0)
+const arr = Array.from({length: Math.abs(b - a) + 1}, (_,v)=> v+a).filter(i => i % 5 === 0)
 const h = arr.reduce((acc,cur)=> acc+=cur ,0)
 
 console.log(h)
