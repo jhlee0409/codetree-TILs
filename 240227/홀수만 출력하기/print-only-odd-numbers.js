3dad61fd762bab44ae3d3b5595332e416c66b4c3
Jack <60695299+jhlee0409@users.noreply.github.com>
2024-02-27 18:15:24 +0900

    Title: 홀수만 출력하기, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

--- 240227/홀수만 출력하기/print-only-odd-numbers.js
+++ 240227/홀수만 출력하기/print-only-odd-numbers.js
@@ -3,4 +3,4 @@ let [n,...arr] = fs.readFileSync(0).toString().split('\n').map(v=> +v)
 
 const r = arr.filter(i=>i % 2 === 1 && i % 3 === 0)
 
-console.log([...(new Set(r))].join('\n'))
+console.log(r.join('\n'))
