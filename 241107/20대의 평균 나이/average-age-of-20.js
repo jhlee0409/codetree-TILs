9f95c5f4d90d722969edadb46603b4d9f38d4a4f
jack <60695299+jhlee0409@users.noreply.github.com>
2024-11-07 11:02:17 +0900

    Title: 20대의 평균 나이, Time: 184ms, Memory: 14MB, Status: Passed - CodeTree

--- 241107/20대의 평균 나이/average-age-of-20.js
+++ 241107/20대의 평균 나이/average-age-of-20.js
@@ -3,5 +3,5 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
 const found30 = input.findIndex(n => n >= 30)
 input.splice(found30,1)
-const result = input.reduce((acc,cur)=> acc+cur, 0) / found30
+const result = input.reduce((acc,cur)=> acc+cur, 0) / input.length
 console.log(result.toFixed(2).trim())
